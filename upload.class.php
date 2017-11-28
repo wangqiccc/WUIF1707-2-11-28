267c2945108e36ea11c869c13fa2b4b9262afecf
qq342222356 <342222356@qq.com>
2017-11-29 00:11:59 +0800

    付少波

---
+++ upload.class.php
@@ -0,0 +1,57 @@
+<?php
+class Upload{
+    public $data;//接收上传文件
+    public $type=["image/png","image/jpg","image/gif","image/jpeg"];
+    public $filename="";
+    public $pathname="";
+    public $fullname="";
+    function __construct($f)
+    {
+        $this->data=$_FILES[$f];
+        $this->size=1024*1024*10;
+    }
+
+    function getfilename()
+    {
+//        echo $this->data["name"];
+        $arr = explode(".",$this->data["name"]);
+        $houzhui=array_pop($arr);//取数组中的最后一个数
+        $this->filename=md5(time()).".".$houzhui;
+
+    }
+    function getpathname(){
+        if(!is_dir("upload")){
+            mkdir("upload");
+        }
+        $this->pathname="upload"."/".date("Y-m-d")."/";
+        if(!is_dir($this->pathname)){
+            mkdir($this->pathname);
+        }
+        $this->fullname=$this->pathname.$this->filename;
+    }
+    function check(){
+        if($this->data["size"]>$this->size){
+            return false;
+        }
+        if(!in_array($this->data["type"],$this->type)){
+            return false;
+        }
+        return true;
+    }
+    function move(){
+        if(is_uploaded_file($this->data["tmp_name"])){
+            move_uploaded_file($this->data["tmp_name"],$this->fullname);
+            echo $this->fullname;
+        }
+    }
+    function upload1(){
+        $this->getfilename();
+        $this->getpathname();
+        if($this->check()){
+            $this->move();
+        }
+    }
+}
+$upl = new Upload("f");
+$upl->upload1();
+?>
